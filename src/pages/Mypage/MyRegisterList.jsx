960422d1020b66aa5a28699676c07fe9fad5be61
Seongbin <csb1320@naver.com>
2024-05-17 11:40:40 +0900

    [05/17] 리뷰 등록, My 리뷰 수정

--- src/pages/Mypage/MyRegisterList.jsx
+++ src/pages/Mypage/MyRegisterList.jsx
@@ -7,6 +7,7 @@ import { useNavigate } from 'react-router-dom';
 import Pagination, {PaginationContainer} from '../Pagination';
 import ConfirmOrCancleModal from '../ConfirmOrCancleModal';
 import ConfirmOrCancleModalDetail from '../ConfirmOrCancleModalDetail';
+import ReviewModal from '../ReviewModalToRetner'
 
 const LocalPaginationContainer = styled(PaginationContainer)`
   justify-content: flex-end;
@@ -23,6 +24,8 @@ const MyRegisterList = () => {
   const [showModal, setShowModal] = useState(false);
   const [processingItemId, setProcessingItemId] = useState(null);
   const [modalOpen, setModalOpen] = useState(false);
+  const [reviewModalOpen, setReviewModalOpen] = useState(false);
+
 
   const [statusData, setStatusData] = useState({
     ALL: { items: [], totalPages: 0 },
@@ -147,6 +150,21 @@ const MyRegisterList = () => {
   const handleRent = (itemId) => {
     setProcessingItemId(itemId);
     setModalOpen(true);
+  };
+
+  const handleConfirm = () => {
+    setModalOpen(false);  // 기존 모달을 닫고
+    setReviewModalOpen(true);  // 리뷰 모달을 엽니다.
+  };
+
+  const goBackToConfirm = () => {
+    setReviewModalOpen(false); // 리뷰 모달 닫기
+    setModalOpen(true); // 이전 모달 열기
+  };
+
+  const handleCloseAllModals = () => {
+    setModalOpen(false);
+    setReviewModalOpen(false);
   };
 
   const confirmRent = async () => {
@@ -290,6 +308,30 @@ const MyRegisterList = () => {
                       )}
                     </>
                   )}
+                  {item.rentingStatus === 'RETURNED' && (
+                      <>
+                        <Handlebutton onClick={() => handleRent(item.id)}>리뷰 쓰기</Handlebutton>
+                        {modalOpen && (
+                          <ConfirmOrCancleModalDetail
+                            title="리뷰를 작성하시겠습니까?"
+                            message={<span>대여자에 대한 별점을 주셔야 <br/>다른 물건 등록이 가능합니다</span>}                          
+                            isOpen={modalOpen}
+                            setIsOpen={setModalOpen}
+                            onConfirm={handleConfirm}
+                          />
+                        )}
+                        {reviewModalOpen && (
+                          <ReviewModal
+                            onBack={goBackToConfirm}
+                            isOpen={reviewModalOpen}
+                            setIsOpen={setReviewModalOpen}
+                            reservationId={processingItemId}
+                            handleCloseAllModals={handleCloseAllModals}
+                            // 여기에 리뷰 모달에 필요한 추가적인 props를 전달할 수 있습니다.
+                          />
+                        )}
+                      </>
+                    )}
                 </ItemStatusDetail>
               </ItemContainer>
             ))}
--- src/pages/Mypage/MyReview.jsx
+++ src/pages/Mypage/MyReview.jsx
@@ -10,6 +10,9 @@ const MyReview = () => {
   const [userNickname, setUserNickname] = useState('');
   const [profileImage, setProfileImage] = useState('');
   const [averageScore, setAverageScore] = useState(0); // 평균 점수 상태 추가
+  const [activeReviewType, setActiveReviewType] = useState('rental');
+  const [isActiveMyReview, setIsActiveMyReview] = useState(true);
+  const [reviews, setReviews] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -40,10 +43,51 @@ const MyReview = () => {
       // 두 점수 모두 유효하지 않을 때
       setAverageScore('0.0');
     }
-  });
+
+    fetchReviews();
+  } ,[isActiveMyReview, activeReviewType]);
+
+  const fetchReviews = async () => {
+    let url = '';
+    if (isActiveMyReview) {
+      url = activeReviewType === 'rental' ? '/review/getCreatedReviewToOwnerByMe' : '/review/getCreatedReviewToRenterByMe';
+    } else {
+      url = activeReviewType === 'rental' ? '/review/getOtherCreateReviewToMeInMyRegisterItem' : '/review/getOtherCreateReviewToMeInMyRentedItem';
+    }
+    const response = await apiClient.get(url);
+    setReviews(response.data.content); // 예시로 .content를 사용했습니다.
+  };
+
+  const isOwnerEndpoint = () => {
+    return (isActiveMyReview && activeReviewType === 'rental') || (!isActiveMyReview && activeReviewType !== 'rental');
+  };
+
+  const handleReviewTypeChange = (type) => {
+    setActiveReviewType(type);
+  };
+
+  const handleReviewSectionChange = (isMyReview) => {
+    setIsActiveMyReview(isMyReview);
+  };
 
   const handlePageChange = (path) => {
     navigate(path);
+  };
+
+  const StarRating = ({ score }) => {
+    const fullStars = Math.floor(score);
+    const emptyStars = 5 - fullStars;
+  
+    return (
+      <div>
+        {Array.from({ length: fullStars }, (_, index) => (
+          <img key={`full-${index}`} src="/assets/img/YellowStar.png" alt="Full Star" style={{ width: '20px' }} />
+        ))}
+        {Array.from({ length: emptyStars }, (_, index) => (
+          <img key={`empty-${index}`} src="/assets/img/GrayStar.png" alt="Empty Star" style={{ width: '20px' }} />
+        ))}
+      </div>
+    );
   };
 
     return (
@@ -64,13 +108,38 @@ const MyReview = () => {
                 </ProfileContainer>
                 
                 <RentingInfoBox>
-                  <Reserved>대여 후기 0</Reserved>
-                  <Returned>반납 완료 0</Returned>
+                  <Reserved onClick={() => handleReviewSectionChange(true)} isActive={isActiveMyReview}>
+                    내가 쓴 리뷰
+                  </Reserved>
+                  <Returned onClick={() => handleReviewSectionChange(false)} isActive={!isActiveMyReview}>
+                    나에게 쓴 리뷰
+                  </Returned>
                 </RentingInfoBox>
 
                 <RentingTitleContainer>
-                   <RentingTitle>대여후기</RentingTitle> {/*대여 후기/거래 후기 선택에 따라 변경 */}
+                   <RentingTitle onClick={() => handleReviewTypeChange('rental')} isActive={activeReviewType === 'rental'}>
+                    대여 후기
+                  </RentingTitle>
+                   <DealTitle onClick={() => handleReviewTypeChange('deal')} isActive={activeReviewType === 'deal'}>
+                    거래 후기
+                  </DealTitle>
                 </RentingTitleContainer>
+
+                <ReviewList>
+                  {reviews.map(review => (
+                    <ReviewItem key={review.reviewId}>
+                      <ProfileItem>
+                        <ProfileImg src={isOwnerEndpoint() ? review.ownerProfile || "/assets/img/Profile.png" : review.renterProfile || "/assets/img/Profile.png"} alt="Profile" />
+                        <NickNameAndRating>
+                          <UserNickname>{isOwnerEndpoint() ? review.ownerNickName : review.renterNickName}</UserNickname>
+                          <StarRating score={review.reviewScore} />
+                        </NickNameAndRating>
+                      </ProfileItem>
+                      <ItemTitle>c타입 충전기 </ItemTitle>
+                      <Comment>{review.reviewComment}</Comment>
+                    </ReviewItem>
+                  ))}
+                </ReviewList>
 
             </Container>
       </ >
@@ -86,52 +155,93 @@ export const GlobalStyle = createGlobalStyle`
       padding: 0;
       display: flex;
       flex-direction: column;
+      font-family: "Pretendard";
       background-color: #000; // body 전체의 배경색을 검은색으로 설정
+  }
+
+  /* 스크롤바 전체 스타일 */
+  ::-webkit-scrollbar {
+    width: 0.5rem;
+  }
+
+  /* 스크롤바 트랙(바탕) 스타일 */
+  ::-webkit-scrollbar-track {
+    background: transparent; /* 트랙의 배경색 */
+  }
+
+  /* 스크롤바 핸들(움직이는 부분) 스타일 */
+  ::-webkit-scrollbar-thumb {
+    background: #00FFE0; /* 핸들의 배경색 */
+    border-radius: 5px;
   }
 `;
 
 const RentingTitleContainer = styled.div`
   display: flex;
-  align-items: center;
+  flex-direction: row;
   margin-top: 2rem;
 `;
 
-const RentingTitle = styled.div`
-  color: white;
+const RentingTitle = styled.button`
+  color: ${(props) => (props.isActive ? '#00FFE0' : 'white')};
+  background-color: transparent;
+  border: none;
   font-family: "Pretendard";
-  font-size: 1.5rem;
+  font-size: 1.3rem;
   font-style: normal;
   font-weight: 700;
-  margin-right: 38rem;
+  margin-left: -22rem;
+  width: 7rem;
+  border-right: 1px solid white;
+  cursor: pointer;
+`;
+
+const DealTitle = styled.button`
+  color: ${(props) => (props.isActive ? '#00FFE0' : 'white')};
+  background-color: transparent;
+  border: none;
+  font-family: "Pretendard";
+  font-size: 1.3rem;
+  font-style: normal;
+  font-weight: 700;
+  margin-left: 2rem;
+  cursor: pointer;
 `;
 
 const RentingInfoBox = styled.div`
-  background: #343434;
+  background: #1F1F1F;
   display: flex;
   justify-content: space-between;
   width: 59.5rem;
-  height: 2.5rem;
+  height: 4rem;
   margin-top: 0rem;
   margin-left: 15rem;
   align-items: center;
-  border-radius: 12px;
+  border-radius: 10px;
 `;
 
-const Reserved = styled.div`
+const Reserved = styled.button`
   flex-grow: 1; /* 자식 요소들의 너비를 동일하게 설정 */
-  color: white;
-  text-align: center;
+  height: 100%;
+  background-color: transparent;
+  border: none;
   font-size: 1.2rem;
   font-weight: 800;
-  border-right: 1px solid #FFF;
+  cursor: pointer;
+  color: ${(props) => (props.isActive ? '#00FFE0' : 'white')};
+  border-bottom: ${(props) => (props.isActive ? '2px solid #00FFE0' : 'none')};
 `;
 
-const Returned = styled.div`
+const Returned = styled.button`
   flex-grow: 1; /* 자식 요소들의 너비를 동일하게 설정 */
-  color: white;
-  text-align: center;
+  height: 100%;
+  background-color: transparent;
+  border: none;
   font-size: 1.2rem;
   font-weight: 800;
+  cursor: pointer;
+  color: ${(props) => (props.isActive ? '#00FFE0' : 'white')};
+  border-bottom: ${(props) => (props.isActive ? '2px solid #00FFE0' : 'none')};
 `;
 
 const Container = styled.div`
@@ -204,3 +314,64 @@ const Nickname = styled.span`
   font-weight: 600;
   margin-top: 1rem;
 `;
+
+const ReviewList = styled.div`
+`;
+
+const ProfileItem = styled.div `
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const NickNameAndRating = styled.div `
+  display: flex;
+  flex-direction: column;
+`;
+
+const UserNickname = styled.span`
+  color: white;
+  font-family: "Pretendard";
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 500;
+  margin-bottom: 0.3rem;
+`;
+
+const ReviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: transparent;
+  margin-top: 3rem;
+  width: 60rem;
+  margin-left: 15rem;
+  border: none;
+  border-bottom: 2px solid #5A5A5A; /* 각 리뷰 항목 아래에 구분선 추가 */
+`;
+
+const ProfileImg = styled.img`
+  width: 3.5rem; 
+  height: 3.5rem;
+  border-radius: 50%; 
+  margin-right: 20px;
+`;
+
+const ItemTitle = styled.button`
+  font-size: 1rem;
+  color: white; /* 아이템 제목 색상 */
+  margin-top: 10px;
+  height: 2rem;
+  border: 1px solid #B2B2B2;
+  background-color: transparent;
+  display: inline-block; /* 내용에 따라 너비가 조정되도록 */
+  white-space: nowrap;
+  max-width: 10rem;
+`;
+
+const Comment = styled.p`
+  font-size: 16px;
+  color: #CCC; /* 리뷰 내용 색상 */
+  margin-top: 10px;
+  white-space: pre-wrap; /* 내용에 줄바꿈 적용 */
+`;
+
